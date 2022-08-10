36089fa320abd930a5eacb78928c44f122fd8777
abhijeet <redefinedabhi2@gmail.com>
2022-08-10 20:25:48 +0530

    Adding Splash Screen Activity, replacing default colors

--- app/src/main/java/com/theleafapps/pro/swirlchat/ui/SplashActivity.kt
+++ app/src/main/java/com/theleafapps/pro/swirlchat/ui/SplashActivity.kt
@@ -1,11 +1,12 @@
-package com.theleafapps.pro.swirlchat
+package com.theleafapps.pro.swirlchat.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.theleafapps.pro.swirlchat.R
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
     }
 }
