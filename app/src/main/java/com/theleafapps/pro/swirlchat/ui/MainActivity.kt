4c55fba3b8da9fea3f539ce49460497359911e43
abhijeet <redefinedabhi2@gmail.com>
2022-08-13 00:03:03 +0530

    Added Data Binding & View Binding in gradle
    Boilerplate code removed from fragments

--- app/src/main/java/com/theleafapps/pro/swirlchat/ui/MainActivity.kt
+++ app/src/main/java/com/theleafapps/pro/swirlchat/ui/MainActivity.kt
@@ -3,10 +3,14 @@ package com.theleafapps.pro.swirlchat.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.theleafapps.pro.swirlchat.R
+import com.theleafapps.pro.swirlchat.ui.fragments.GetUserNumber
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportFragmentManager.beginTransaction()
+            .add(R.id.main_container,GetUserNumber())
+            .commit()
     }
 }
