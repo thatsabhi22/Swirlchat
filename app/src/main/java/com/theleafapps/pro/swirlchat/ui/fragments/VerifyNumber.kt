cde570156033eb277d646e8916c30f9f66337490
abhijeet <redefinedabhi2@gmail.com>
2022-08-15 02:47:01 +0530

    Setting up verifynumber fragment

--- app/src/main/java/com/theleafapps/pro/swirlchat/ui/fragments/VerifyNumber.kt
+++ app/src/main/java/com/theleafapps/pro/swirlchat/ui/fragments/VerifyNumber.kt
@@ -5,15 +5,76 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthProvider
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import com.theleafapps.pro.swirlchat.R
+import com.theleafapps.pro.swirlchat.databinding.FragmentGetUserNumberBinding
+import com.theleafapps.pro.swirlchat.databinding.FragmentVerifyNumberBinding
+import com.theleafapps.pro.swirlchat.entities.UserModel
 
 class VerifyNumber : Fragment() {
+
+    private var code: String? = null
+    private lateinit var pin: String
+    private var firebaseAuth: FirebaseAuth? = null
+    private var databaseReference: DatabaseReference? = null
+    private var binding: FragmentVerifyNumberBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            code = it.getString("Code")
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_verify_number, container, false)
+        val view =inflater.inflate(R.layout.fragment_verify_number, container, false)
+        binding = FragmentVerifyNumberBinding.inflate(inflater,container,false)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+        databaseReference = FirebaseDatabase.getInstance().getReference("Users")
+        binding?.btnVerify?.setOnClickListener {
+            if (checkPin()) {
+                val credential = PhoneAuthProvider.getCredential(code!!, pin)
+                signInUser(credential)
+            }
+        }
+        return binding?.root
+    }
+
+    private fun signInUser(credential: PhoneAuthCredential) {
+        firebaseAuth!!.signInWithCredential(credential).addOnCompleteListener {
+            if (it.isSuccessful) {
+                val userModel =
+                    UserModel(
+                        "", "", "",
+                        firebaseAuth!!.currentUser!!.phoneNumber!!,
+                        firebaseAuth!!.uid!!
+                    )
+
+                databaseReference!!.child(firebaseAuth?.uid!!).setValue(userModel)
+                requireActivity().supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.main_container, GetUserData())
+                    .commit()
+            }
+        }
+    }
+
+    private fun checkPin(): Boolean {
+        pin = binding?.otpTextView?.text.toString()
+        if (pin.isEmpty()) {
+            binding?.otpTextView?.error = "Filed is required"
+            return false
+        } else if (pin.length < 6) {
+            binding?.otpTextView?.error = "Enter valid pin"
+            return false
+        } else return true
     }
 }
