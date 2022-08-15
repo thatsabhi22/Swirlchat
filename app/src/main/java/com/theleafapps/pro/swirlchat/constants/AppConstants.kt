0130907601670a389b45c53579292540a4b0d4a1
abhijeet <redefinedabhi2@gmail.com>
2022-08-16 03:58:39 +0530

    Adding logic for get user data fragment

---
+++ app/src/main/java/com/theleafapps/pro/swirlchat/constants/AppConstants.kt
@@ -0,0 +1,16 @@
+package com.theleafapps.pro.swirlchat.constants
+
+interface AppConstants {
+
+    companion object {
+
+        const val CHANNEL_ID = "Message Channel"
+        const val SERVER_KEY=""
+        const val NOTIFICATION_URL = "https://fcm.googleapis.com/fcm/send"
+        const val PATH = "/ProfileImage/image"
+        const val UID = ""
+        const val STORAGE_PERMISSION = 1000
+        const val CONTACT_PERMISSION = 2000
+
+    }
+}
--- app/src/main/java/com/theleafapps/pro/swirlchat/ui/fragments/GetUserNumber.kt
+++ app/src/main/java/com/theleafapps/pro/swirlchat/ui/fragments/GetUserNumber.kt
@@ -1,111 +1,141 @@
 package com.theleafapps.pro.swirlchat.ui.fragments
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import com.google.firebase.FirebaseException
-import com.google.firebase.FirebaseTooManyRequestsException
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.canhub.cropper.CropImage
+import com.canhub.cropper.CropImageView
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
-import com.google.firebase.auth.PhoneAuthCredential
-import com.google.firebase.auth.PhoneAuthProvider
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 import com.theleafapps.pro.swirlchat.R
-import com.theleafapps.pro.swirlchat.databinding.FragmentGetUserNumberBinding
-import com.theleafapps.pro.swirlchat.entities.UserModel
-import java.util.concurrent.TimeUnit
+import com.theleafapps.pro.swirlchat.databinding.FragmentGetUserDataBinding
 
-class GetUserNumber : Fragment() {
+class GetUserData : Fragment() {
 
-    private var number: String? = null
-    private lateinit var callbacks: PhoneAuthProvider.OnVerificationStateChangedCallbacks
-    private var code: String? = null
-    private var firebaseAuth: FirebaseAuth? = null
+    private var image: Uri? = null
+    private lateinit var username: String
+    private lateinit var status: String
+    private lateinit var imageUrl: String
     private var databaseReference: DatabaseReference? = null
-    private var binding: FragmentGetUserNumberBinding? = null
+    private var firebaseAuth: FirebaseAuth? = null
+    private var storageReference: StorageReference? = null
+    private var binding: FragmentGetUserDataBinding? = null
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_get_user_number, container, false)
-        binding = FragmentGetUserNumberBinding.inflate(inflater,container,false)
+        val view =  inflater.inflate(R.layout.fragment_get_user_data, container, false)
+        binding = FragmentGetUserDataBinding.inflate(inflater,container,false)
 
         firebaseAuth = FirebaseAuth.getInstance()
         databaseReference = FirebaseDatabase.getInstance().getReference("Users")
-        binding?.btnGenerateOTP?.setOnClickListener {
-            if (checkNumber()) {
-                val phoneNumber = binding?.countryCodePicker?.selectedCountryCodeWithPlus + number
-                sendCode(phoneNumber)
+        storageReference = FirebaseStorage.getInstance().reference
+
+        binding?.btnDataDone?.setOnClickListener {
+            if (checkData()) {
+                uploadData(username, status, image!!)
             }
         }
 
-        callbacks = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
-
-            override fun onVerificationCompleted(credential: PhoneAuthCredential) {
-                firebaseAuth!!.signInWithCredential(credential).addOnCompleteListener {
-                    if (it.isSuccessful) {
-                        val userModel =
-                            UserModel(
-                                "", "", "",
-                                firebaseAuth!!.currentUser!!.phoneNumber!!,
-                                firebaseAuth!!.uid!!
-                            )
-
-                        databaseReference!!.child(firebaseAuth!!.uid!!).setValue(userModel)
-                        activity?.supportFragmentManager
-                            ?.beginTransaction()
-                            ?.replace(R.id.main_container, GetUserData())
-                            ?.commit()
-                    }
-                }
-            }
-
-            override fun onVerificationFailed(e: FirebaseException) {
-                if (e is FirebaseAuthInvalidCredentialsException)
-                    Toast.makeText(context, "" + e.message, Toast.LENGTH_SHORT).show()
-                else if (e is FirebaseTooManyRequestsException)
-                    Toast.makeText(context, "" + e.message, Toast.LENGTH_SHORT).show()
-                else Toast.makeText(context, "" + e.message, Toast.LENGTH_SHORT).show()
-            }
-
-            override fun onCodeSent(
-                verificationCode: String,
-                p1: PhoneAuthProvider.ForceResendingToken
-            ) {
-                code = verificationCode
-                activity!!.supportFragmentManager
-                    .beginTransaction()
-                    .replace(R.id.main_container, VerifyNumber.newInstance(code!!))
-                    .commit()
-            }
-        }
-        return binding?.root
+        return view
     }
 
-    private fun sendCode(phoneNumber: String) {
-        PhoneAuthProvider.getInstance().verifyPhoneNumber(
-            phoneNumber,
-            60,
-            TimeUnit.SECONDS,
-            requireActivity(),
-            callbacks
-        )
+    private fun checkData(): Boolean {
+        username = binding?.edtUserName?.text.toString().trim()
+        status = binding?.edtUserStatus?.text.toString().trim()
 
-    }
-
-    private fun checkNumber(): Boolean {
-        number = binding?.edtNumber?.text.toString().trim()
-        if (number!!.isEmpty()) {
-            binding?.edtNumber?.error = "Field is required"
+        if (username.isEmpty()) {
+            binding?.edtUserName?.error = "Filed is required"
             return false
-        } else if (number!!.length < 10) {
-            binding?.edtNumber?.error = "Number should be 10 in length"
+        } else if (status.isEmpty()) {
+            binding?.edtUserStatus?.error = "Filed is required"
+            return false
+        } else if (image == null) {
+            Toast.makeText(context, "Image required", Toast.LENGTH_SHORT).show()
             return false
         } else return true
+    }
+
+    private fun uploadData(name: String, status: String, image: Uri) = kotlin.run {
+        storageReference!!.child(firebaseAuth!!.uid + AppConstants.PATH).putFile(image)
+            .addOnSuccessListener {
+                val task = it.storage.downloadUrl
+                task.addOnCompleteListener { uri ->
+                    imageUrl = uri.result.toString()
+                    val map = mapOf(
+                        "name" to name,
+                        "status" to status,
+                        "image" to imageUrl
+                    )
+                    databaseReference!!.child(firebaseAuth!!.uid!!).updateChildren(map)
+
+                    startActivity(Intent(context, DashBoard::class.java))
+                    requireActivity().finish()
+                }
+            }
+    }
+
+    private fun checkStoragePermission(): Boolean {
+        return ContextCompat.checkSelfPermission(
+            requireContext(),
+            Manifest.permission.READ_EXTERNAL_STORAGE
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun storageRequestPermission() = ActivityCompat.requestPermissions(
+        requireActivity(),
+        arrayOf(
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        ), 1000
+    )
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            1000 ->
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    pickImage()
+                else Toast.makeText(context, "Storage permission denied", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+
+            CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE -> {
+                val result = CropImage.getActivityResult(data)
+                if (resultCode == Activity.RESULT_OK) {
+                    image = result.uri
+                    binding?.imgUser?.setImageURI(image)
+                }
+            }
+        }
+    }
+
+    private fun pickImage() {
+        CropImage.activity()
+            .setCropShape(CropImageView.CropShape.OVAL)
+            .start(requireContext(), this)
     }
 }
