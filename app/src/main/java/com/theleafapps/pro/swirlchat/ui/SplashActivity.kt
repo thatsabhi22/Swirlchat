05cc268a60fbc9096b052f6cb4265ef861ff92f9
abhijeet <redefinedabhi2@gmail.com>
2022-08-16 04:53:52 +0530

    Adding AppUtil for utility functions, enabling Splash Screen

--- app/src/main/java/com/theleafapps/pro/swirlchat/ui/SplashActivity.kt
+++ app/src/main/java/com/theleafapps/pro/swirlchat/ui/SplashActivity.kt
@@ -1,12 +1,49 @@
 package com.theleafapps.pro.swirlchat.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 import com.theleafapps.pro.swirlchat.R
+import com.theleafapps.pro.swirlchat.util.AppUtil
 
 class SplashActivity : AppCompatActivity() {
+
+    private var firebaseAuth: FirebaseAuth? = null
+    private lateinit var appUtil: AppUtil
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        firebaseAuth = FirebaseAuth.getInstance()
+        appUtil = AppUtil()
+
+        Handler().postDelayed({
+
+            if (firebaseAuth!!.currentUser == null) {
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            } else {
+
+                FirebaseInstanceId.getInstance().instanceId
+                    .addOnCompleteListener(OnCompleteListener {
+                        if (it.isSuccessful) {
+                            val token = it.result?.token
+                            val databaseReference =
+                                FirebaseDatabase.getInstance().getReference("Users")
+                                    .child(appUtil.getUID()!!)
+
+                            val map: MutableMap<String, Any> = HashMap()
+                            map["token"] = token!!
+                            databaseReference.updateChildren(map)
+                        }
+                    })
+                startActivity(Intent(this, DashBoard::class.java))
+                finish()
+            }
+
+        }, 3000)
     }
 }
--- app/src/main/java/com/theleafapps/pro/swirlchat/ui/fragments/GetUserData.kt
+++ app/src/main/java/com/theleafapps/pro/swirlchat/ui/fragments/GetUserData.kt
@@ -35,7 +35,6 @@ class GetUserData : Fragment() {
     private var storageReference: StorageReference? = null
     private var binding: FragmentGetUserDataBinding? = null
 
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -43,100 +42,100 @@ class GetUserData : Fragment() {
         val view =  inflater.inflate(R.layout.fragment_get_user_data, container, false)
         binding = FragmentGetUserDataBinding.inflate(inflater,container,false)
 
-        firebaseAuth = FirebaseAuth.getInstance()
-        databaseReference = FirebaseDatabase.getInstance().getReference("Users")
-        storageReference = FirebaseStorage.getInstance().reference
-
-        binding?.btnDataDone?.setOnClickListener {
-            if (checkData()) {
-                uploadData(username, status, image!!)
-            }
-        }
+//        firebaseAuth = FirebaseAuth.getInstance()
+//        databaseReference = FirebaseDatabase.getInstance().getReference("Users")
+//        storageReference = FirebaseStorage.getInstance().reference
+//
+//        binding?.btnDataDone?.setOnClickListener {
+//            if (checkData()) {
+//                uploadData(username, status, image!!)
+//            }
+//        }
 
         return view
     }
 
-    private fun checkData(): Boolean {
-        username = binding?.edtUserName?.text.toString().trim()
-        status = binding?.edtUserStatus?.text.toString().trim()
-
-        if (username.isEmpty()) {
-            binding?.edtUserName?.error = "Filed is required"
-            return false
-        } else if (status.isEmpty()) {
-            binding?.edtUserStatus?.error = "Filed is required"
-            return false
-        } else if (image == null) {
-            Toast.makeText(context, "Image required", Toast.LENGTH_SHORT).show()
-            return false
-        } else return true
-    }
-
-    private fun uploadData(name: String, status: String, image: Uri) = kotlin.run {
-        storageReference!!.child(firebaseAuth!!.uid + AppConstants.PATH).putFile(image)
-            .addOnSuccessListener {
-                val task = it.storage.downloadUrl
-                task.addOnCompleteListener { uri ->
-                    imageUrl = uri.result.toString()
-                    val map = mapOf(
-                        "name" to name,
-                        "status" to status,
-                        "image" to imageUrl
-                    )
-                    databaseReference!!.child(firebaseAuth!!.uid!!).updateChildren(map)
-
-                    startActivity(Intent(context, DashBoard::class.java))
-                    requireActivity().finish()
-                }
-            }
-    }
-
-    private fun checkStoragePermission(): Boolean {
-        return ContextCompat.checkSelfPermission(
-            requireContext(),
-            Manifest.permission.READ_EXTERNAL_STORAGE
-        ) == PackageManager.PERMISSION_GRANTED
-    }
-
-    private fun storageRequestPermission() = ActivityCompat.requestPermissions(
-        requireActivity(),
-        arrayOf(
-            Manifest.permission.READ_EXTERNAL_STORAGE,
-            Manifest.permission.WRITE_EXTERNAL_STORAGE
-        ), 1000
-    )
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        when (requestCode) {
-            1000 ->
-                if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
-                    pickImage()
-                else Toast.makeText(context, "Storage permission denied", Toast.LENGTH_SHORT).show()
-        }
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        when (requestCode) {
-
-            CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE -> {
-                val result = CropImage.getActivityResult(data)
-                if (resultCode == Activity.RESULT_OK) {
-                    image = result.uri
-                    binding?.imgUser?.setImageURI(image)
-                }
-            }
-        }
-    }
-
-    private fun pickImage() {
-        CropImage.activity()
-            .setCropShape(CropImageView.CropShape.OVAL)
-            .start(requireContext(), this)
-    }
+//    private fun checkData(): Boolean {
+//        username = binding?.edtUserName?.text.toString().trim()
+//        status = binding?.edtUserStatus?.text.toString().trim()
+//
+//        if (username.isEmpty()) {
+//            binding?.edtUserName?.error = "Filed is required"
+//            return false
+//        } else if (status.isEmpty()) {
+//            binding?.edtUserStatus?.error = "Filed is required"
+//            return false
+//        } else if (image == null) {
+//            Toast.makeText(context, "Image required", Toast.LENGTH_SHORT).show()
+//            return false
+//        } else return true
+//    }
+//
+//    private fun uploadData(name: String, status: String, image: Uri) = kotlin.run {
+//        storageReference!!.child(firebaseAuth!!.uid + AppConstants.PATH).putFile(image)
+//            .addOnSuccessListener {
+//                val task = it.storage.downloadUrl
+//                task.addOnCompleteListener { uri ->
+//                    imageUrl = uri.result.toString()
+//                    val map = mapOf(
+//                        "name" to name,
+//                        "status" to status,
+//                        "image" to imageUrl
+//                    )
+//                    databaseReference!!.child(firebaseAuth!!.uid!!).updateChildren(map)
+//
+//                    startActivity(Intent(context, DashBoard::class.java))
+//                    requireActivity().finish()
+//                }
+//            }
+//    }
+//
+//    private fun checkStoragePermission(): Boolean {
+//        return ContextCompat.checkSelfPermission(
+//            requireContext(),
+//            Manifest.permission.READ_EXTERNAL_STORAGE
+//        ) == PackageManager.PERMISSION_GRANTED
+//    }
+//
+//    private fun storageRequestPermission() = ActivityCompat.requestPermissions(
+//        requireActivity(),
+//        arrayOf(
+//            Manifest.permission.READ_EXTERNAL_STORAGE,
+//            Manifest.permission.WRITE_EXTERNAL_STORAGE
+//        ), 1000
+//    )
+//
+//    override fun onRequestPermissionsResult(
+//        requestCode: Int,
+//        permissions: Array<out String>,
+//        grantResults: IntArray
+//    ) {
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+//        when (requestCode) {
+//            1000 ->
+//                if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
+//                    pickImage()
+//                else Toast.makeText(context, "Storage permission denied", Toast.LENGTH_SHORT).show()
+//        }
+//    }
+//
+//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+//        super.onActivityResult(requestCode, resultCode, data)
+//        when (requestCode) {
+//
+//            CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE -> {
+//                val result = CropImage.getActivityResult(data)
+//                if (resultCode == Activity.RESULT_OK) {
+//                    image = result.uri
+//                    binding?.imgUser?.setImageURI(image)
+//                }
+//            }
+//        }
+//    }
+//
+//    private fun pickImage() {
+//        CropImage.activity()
+//            .setCropShape(CropImageView.CropShape.OVAL)
+//            .start(requireContext(), this)
+//    }
 }
