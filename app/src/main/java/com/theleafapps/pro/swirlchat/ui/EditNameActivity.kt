b3139cebc850ccc5d5dce6c94842a4d6436af88e
abhijeet <redefinedabhi2@gmail.com>
2022-08-16 05:47:44 +0530

    Adding activity to edit user's name, layout designed

---
+++ app/src/main/java/com/theleafapps/pro/swirlchat/ui/EditNameActivity.kt
@@ -0,0 +1,12 @@
+package com.theleafapps.pro.swirlchat.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.theleafapps.pro.swirlchat.R
+
+class EditNameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_name)
+    }
+}
