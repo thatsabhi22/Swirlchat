4959fe31b7a2ae68954dfc74d9173c2dbae9145f
abhijeet <redefinedabhi2@gmail.com>
2022-08-18 06:25:45 +0530

    Update DashboardActivity.kt
    Adding DashboardActivity logic

--- app/src/main/java/com/theleafapps/pro/swirlchat/ui/DashboardActivity.kt
+++ app/src/main/java/com/theleafapps/pro/swirlchat/ui/DashboardActivity.kt
@@ -2,11 +2,53 @@ package com.theleafapps.pro.swirlchat.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.theleafapps.pro.swirlchat.R
+import com.theleafapps.pro.swirlchat.util.AppUtil
 
 class DashboardActivity : AppCompatActivity() {
+
+    private var fragment: Fragment? = null
+    private lateinit var appUtil: AppUtil
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
+        appUtil = AppUtil()
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.dashboardContainer, ChatFragment()).commit()
+            bottomChip.setItemSelected(R.id.btnChat)
+        }
+
+        bottomChip.setOnItemSelectedListener { id ->
+            when (id) {
+                R.id.btnChat -> {
+                    fragment = ChatFragment()
+                }
+
+                R.id.btnProfile -> {
+                    fragment = ProfileFragment();
+                }
+                R.id.btnContact -> fragment = ContactFragment()
+            }
+
+            fragment!!.let {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.dashboardContainer, fragment!!)
+                    .commit()
+            }
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        appUtil.updateOnlineStatus("offline")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        appUtil.updateOnlineStatus("online")
     }
 }
