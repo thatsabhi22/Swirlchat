d0d97dab0b72089ab6dd023156549283f91c4df5
abhijeet <redefinedabhi2@gmail.com>
2022-08-18 06:30:08 +0530

    adding viewbinding to the dashboard activity

--- app/src/main/java/com/theleafapps/pro/swirlchat/ui/DashboardActivity.kt
+++ app/src/main/java/com/theleafapps/pro/swirlchat/ui/DashboardActivity.kt
@@ -4,25 +4,29 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import com.theleafapps.pro.swirlchat.R
+import com.theleafapps.pro.swirlchat.databinding.ActivityDashboardBinding
 import com.theleafapps.pro.swirlchat.util.AppUtil
 
 class DashboardActivity : AppCompatActivity() {
 
     private var fragment: Fragment? = null
+    private var dashboardBinding: ActivityDashboardBinding? = null
     private lateinit var appUtil: AppUtil
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        dashboardBinding = ActivityDashboardBinding.inflate(layoutInflater)
+        setContentView(dashboardBinding?.root)
         setContentView(R.layout.activity_dashboard)
         appUtil = AppUtil()
 
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.dashboardContainer, ChatFragment()).commit()
-            bottomChip.setItemSelected(R.id.btnChat)
+            dashboardBinding?.bottomChip?.setItemSelected(R.id.btnChat)
         }
 
-        bottomChip.setOnItemSelectedListener { id ->
+        dashboardBinding?.bottomChip?.setOnItemSelectedListener { id ->
             when (id) {
                 R.id.btnChat -> {
                     fragment = ChatFragment()
