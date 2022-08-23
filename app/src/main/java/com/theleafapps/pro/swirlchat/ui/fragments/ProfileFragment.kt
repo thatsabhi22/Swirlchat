9d3c4357902aff5800c9b89d17b741d91a041793
abhijeet <redefinedabhi2@gmail.com>
2022-08-24 04:05:23 +0530

    Update ProfileFragment.kt
    added binding and firebase initialisation

--- app/src/main/java/com/theleafapps/pro/swirlchat/ui/fragments/ProfileFragment.kt
+++ app/src/main/java/com/theleafapps/pro/swirlchat/ui/fragments/ProfileFragment.kt
@@ -1,60 +1,44 @@
 package com.theleafapps.pro.swirlchat.ui.fragments
 
+import android.app.AlertDialog
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.storage.StorageReference
 import com.theleafapps.pro.swirlchat.R
+import com.theleafapps.pro.swirlchat.databinding.FragmentProfileBinding
+import com.theleafapps.pro.swirlchat.permission.AppPermission
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ProfileFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ProfileFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var profileBinding: FragmentProfileBinding
+    private lateinit var profileViewModels: ProfileViewModel
+    private lateinit var dialog: AlertDialog
+    private lateinit var appPermission: AppPermission
+    private lateinit var storageReference: StorageReference
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var sharedPreferences: SharedPreferences
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+
+        profileBinding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_profile, container, false)
+
+        appPermission = AppPermission()
+        firebaseAuth = FirebaseAuth.getInstance()
+        sharedPreferences = requireContext().getSharedPreferences("userData", Context.MODE_PRIVATE)
+
+        return profileBinding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ProfileFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ProfileFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
