9f0df7c901ced69e0b5fa7f74aab09600d5d8573
abhijeet <redefinedabhi2@gmail.com>
2022-08-25 03:50:10 +0530

    new methods added to profile fragment
    getstatusdialog
    pickimage
    uploadimage

--- app/src/main/java/com/theleafapps/pro/swirlchat/ui/fragments/ProfileFragment.kt
+++ app/src/main/java/com/theleafapps/pro/swirlchat/ui/fragments/ProfileFragment.kt
@@ -1,17 +1,26 @@
 package com.theleafapps.pro.swirlchat.ui.fragments
 
+import android.app.Activity
 import android.app.AlertDialog
 import android.content.Context
+import android.content.Intent
 import android.content.SharedPreferences
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import com.canhub.cropper.CropImage
+import com.canhub.cropper.CropImageView
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
 import com.theleafapps.pro.swirlchat.R
+import com.theleafapps.pro.swirlchat.constants.AppConstants
 import com.theleafapps.pro.swirlchat.databinding.FragmentProfileBinding
 import com.theleafapps.pro.swirlchat.permission.AppPermission
 
@@ -41,4 +50,85 @@ class ProfileFragment : Fragment() {
         return profileBinding.root
     }
 
+    private fun pickImage() {
+        CropImage.activity().setCropShape(CropImageView.CropShape.OVAL)
+            .start(requireContext(), this)
+    }
+
+    private fun getStatusDialog() {
+
+        val alertDialog = AlertDialog.Builder(context)
+        val view = LayoutInflater.from(context).inflate(R.layout.dialog_layout, null, false)
+        alertDialog.setView(view)
+
+        view.btnEditStatus.setOnClickListener {
+            val status = view.edtUserStatus.text.toString()
+            if (status.isNotEmpty()) {
+                profileViewModels.updateStatus(status)
+                dialog.dismiss()
+            }
+        }
+        dialog = alertDialog.create()
+        dialog.show()
+
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            100 -> {
+                if (data != null) {
+                    val userName = data.getStringExtra("name")
+                    profileViewModels.updateName(userName!!)
+                    val editor = sharedPreferences.edit()
+                    editor.putString("myName", userName).apply()
+                }
+            }
+            CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE -> {
+                if (data != null) {
+
+                    val result = CropImage.getActivityResult(data)
+                    if (resultCode == Activity.RESULT_OK) {
+                        uploadImage(result.uri)
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            AppConstants.STORAGE_PERMISSION -> {
+
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    pickImage()
+                else Toast.makeText(context, "Permission Denied", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun uploadImage(imageUri: Uri) {
+
+        storageReference = FirebaseStorage.getInstance().reference
+        storageReference.child(firebaseAuth.uid + AppConstants.PATH).putFile(imageUri)
+            .addOnSuccessListener { taskSnapshot ->
+                val task = taskSnapshot.storage.downloadUrl
+                task.addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        val imagePath = it.result.toString()
+
+                        val editor = sharedPreferences.edit()
+                        editor.putString("myImage", imagePath).apply()
+
+                        profileViewModels.updateImage(imagePath)
+                    }
+                }
+            }
+    }
 }
