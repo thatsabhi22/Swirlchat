985c83fed1d16971a643eb86650b4bd1cffdd404
abhijeet <redefinedabhi2@gmail.com>
2022-09-16 05:08:14 +0530

    Replaced BottomNavigation Library with official

--- app/src/main/java/com/theleafapps/pro/swirlchat/ui/DashboardActivity.kt
+++ app/src/main/java/com/theleafapps/pro/swirlchat/ui/DashboardActivity.kt
@@ -3,7 +3,6 @@ package com.theleafapps.pro.swirlchat.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import com.ismaeldivita.chipnavigation.ChipNavigationBar
 import com.theleafapps.pro.swirlchat.R
 import com.theleafapps.pro.swirlchat.databinding.ActivityDashboardBinding
 import com.theleafapps.pro.swirlchat.ui.fragments.ChatFragment
@@ -14,45 +13,35 @@ import com.theleafapps.pro.swirlchat.util.AppUtil
 class DashboardActivity : AppCompatActivity() {
 
     private var fragment: Fragment? = null
-    private var dashboardBinding: ActivityDashboardBinding? = null
+    private lateinit var dashboardBinding: ActivityDashboardBinding
     private lateinit var appUtil: AppUtil
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         dashboardBinding = ActivityDashboardBinding.inflate(layoutInflater)
-        setContentView(dashboardBinding?.root)
-        setContentView(R.layout.activity_dashboard)
+        setContentView(dashboardBinding.root)
         appUtil = AppUtil()
 
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.dashboardContainer, ChatFragment()).commit()
-            dashboardBinding?.bottomChip?.setItemSelected(R.id.btnChat)
-            dashboardBinding?.bottomChip?.setItemEnabled(R.id.btnChat,true)
         }
 
-        dashboardBinding?.bottomChip?.setOnItemSelectedListener {
-            object : ChipNavigationBar.OnItemSelectedListener {
-                override fun onItemSelected(id: Int) {
-                    when (id) {
-                        R.id.btnChat -> {
-                            fragment = ChatFragment()
-                        }
-                        R.id.btnProfile -> {
-                            fragment = ProfileFragment();
-                        }
-                        R.id.btnContact -> {
-                            fragment = ContactFragment()
-                        }
-                    }
-                    fragment!!.let {
-                        supportFragmentManager.beginTransaction()
-                            .replace(R.id.dashboardContainer, fragment!!)
-                            .commit()
-                    }
-                }
+        dashboardBinding.bottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.btnChat -> replaceFragment(ChatFragment())
+                R.id.btnProfile -> replaceFragment(ProfileFragment())
+                R.id.btnContact -> replaceFragment(ContactFragment())
+                else -> {}
             }
+            true
         }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.dashboardContainer, fragment)
+            .commit()
     }
 
     override fun onPause() {
